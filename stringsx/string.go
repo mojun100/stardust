6751438c04f1783addbac2c0aa83884732ff1f56
mojun <marlud@sina.com>
2022-08-30 11:22:59 +0800

    change Hello function to support lang

--- stringsx/string.go
+++ stringsx/string.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s", name), nil
+func Hello(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "zh":
+		return fmt.Sprintf("你好, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", fmt.Errorf("unknow language")
+	}
 }
